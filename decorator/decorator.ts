18fe0a87c873268c751ca05cae042bd6e061eda8
wuhaolei <wuhaolei@kanyun.com>
2024-11-08 19:23:31 +0800

    ADD: networkExceptionHandler

--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -12,3 +12,29 @@ export function NameLogger(
 
   return descriptor;
 }
+
+export function onNetworkException(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    return new Promise((resolve, reject) => {
+      originalMethod.apply(this, args).then((res) => {
+        resolve(res);
+      }).catch((error) => {
+        handleNetworkException(error);
+        reject(error)
+      })
+    });
+  };
+
+  return descriptor;
+}
+
+function handleNetworkException(error: any) {
+  console.log('Network Exception: ', error);
+}
+
--- decorator/main.ts
+++ decorator/main.ts
@@ -1,10 +1,17 @@
-import { NameLogger } from "./decorator";
+import { NameLogger, onNetworkException } from "./decorator";
 
 class Test {
   @NameLogger
   test() {
     console.log("test");
   }
+
+  @onNetworkException
+  async fetchData(url: string): Promise<any> {
+    return fetch(url)
+  }
 }
 
+
 new Test().test();
+const res = await new Test().fetchData("https://jsonplaceholder.typicode.com/posts/1")
