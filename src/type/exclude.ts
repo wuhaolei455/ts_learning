2c62864b17b9292fd7b2616753bb84159d2a515c
wuhaolei <nell53052@gmail.com>
2025-10-31 00:12:21 +0800

    exclude & pick & omit

--- src/type/exclude.ts
+++ src/type/exclude.ts
@@ -1,1 +1,27 @@
-type MyExclude<T, U> = T extends U ? never : T;
+type MyExclude<T, U> = T extends U ? T : never;
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+type UserNameAndId = MyPick<User, "name" | "id">;
+// // 相当于：
+// // type UserNameAndId = {
+// //   id: number;
+// //   name: string;
+// // }
+
+//
+type MyPick<T, K extends keyof T> = {
+  [M in K]: T[M];
+};
+
+type MyOmit<T, K extends keyof T> = MyPick<T, Exclude<keyof T, K>>;
+type MyOmitUser = MyOmit<User, "email">;
+// // 相当于：
+// // type UserNameAndId = {
+// //   id: number;
+// //   name: string;
+// // }
